e2c1c56fad31324aaff86891fa040537b76e73a7
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 19:27:46 -0300

    feat: added draw function to the example app

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import Canvas from "../../src";
 
-const App = () => {
-  return <Canvas />;
-};
+function App() {
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  return <Canvas draw={draw} />;
+}
 
 render(<App />, document.getElementById("root"));
