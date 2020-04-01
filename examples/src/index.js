85e6567c8ab167c53c0ed523e7a6af03240947fd
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 02:14:56 -0300

    first commit

---
+++ examples/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Canvas from '../../src'
+
+const App = () => {
+  return <Canvas />
+}
+
+render(<App />, document.getElementById("root"))
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Canvas = () => <canvas>Oops, your browser does not support canvas :(</canvas>
+
+export default Canvas 
