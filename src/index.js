4811745f859eed20d65b4f83e40824826e6d61e7
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 19:21:24 -0300

    feat: added animation to draw function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,31 @@ import React, { useRef, useEffect } from "react";
 const Canvas = props => {
   const canvasRef = useRef(null);
 
-  const draw = ctx => {
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.beginPath();
-    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
     ctx.fill();
   };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+    let frameCount = 0;
+    let animationFrameId;
 
-    // draw come here
-    draw(context);
+    //Our draw came here
+    const render = () => {
+      frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
   }, [draw]);
 
   return <canvas ref={canvasRef} {...props} />;
