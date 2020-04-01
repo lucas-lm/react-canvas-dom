250271eb30d2345ea264c3e41b6c1f57d6d65d7c
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 19:16:07 -0300

    feat: added draw to canvas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 const Canvas = props => {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    //Our first draw
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  }, []);
 
   return <canvas ref={canvasRef} {...props} />;
 };
