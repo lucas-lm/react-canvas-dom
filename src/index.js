956618fd637d6eb4afbfddaeabcc45951e04c32f
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 19:19:53 -0300

    feat: added draw function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import React, { useRef, useEffect } from "react";
 const Canvas = props => {
   const canvasRef = useRef(null);
 
+  const draw = ctx => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    //Our first draw
-    context.fillStyle = "#000000";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-  }, []);
+
+    // draw come here
+    draw(context);
+  }, [draw]);
 
   return <canvas ref={canvasRef} {...props} />;
 };
