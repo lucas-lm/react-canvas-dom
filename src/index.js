f54bff681a7b8fff6c5c67531876167302634776
Lucas Miranda <lucas-m@outlook.com>
2020-04-01 19:32:46 -0300

    refactor: extract canvas logic from useCanvas hook

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
+import useCanvas from "./useCanvas";
 
 const Canvas = props => {
   const { draw, ...rest } = props;
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    let frameCount = 0;
-    let animationFrameId;
-
-    const render = () => {
-      frameCount++;
-      draw(context, frameCount);
-      animationFrameId = window.requestAnimationFrame(render);
-    };
-    render();
-
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
-  }, [draw]);
+  const canvasRef = useCanvas(draw);
 
   return <canvas ref={canvasRef} {...rest} />;
 };
