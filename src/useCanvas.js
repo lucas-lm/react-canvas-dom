b84c4e30ebd2ccf5f0c775976f45b5ac281f08c3
Lucas Miranda <lucas-m@outlook.com>
2020-04-02 00:42:39 -0300

    feat: added canvas rescale with device pixel ratio

--- src/useCanvas.js
+++ src/useCanvas.js
@@ -4,8 +4,11 @@ const updateCanvasSize = canvas => {
   const { width, height } = canvas.getBoundingClientRect();
 
   if (canvas.width !== width || canvas.height !== height) {
-    canvas.width = width;
-    canvas.height = height;
+    const { devicePixelRatio: ratio = 1 } = window;
+    const context = canvas.getContext("2d");
+    canvas.width = width * ratio;
+    canvas.height = height * ratio;
+    context.scale(ratio, ratio);
     return true;
   }
 
