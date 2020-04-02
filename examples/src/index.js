5cf61dd259ce151e426be798dd95ac436990ad43
Lucas Miranda <lucas-m@outlook.com>
2020-04-02 01:23:34 -0300

    changed draw function

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,15 +3,22 @@ import { render } from "react-dom";
 import Canvas from "../../src";
 
 function App() {
+  let x, y;
   const draw = (ctx, frameCount) => {
+    if (frameCount === 0 || frameCount % 100 === 0) {
+      x = 100 + Math.random() * 500;
+      y = 100 + Math.random() * 500;
+    }
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.arc(x, y, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
     ctx.fill();
   };
 
-  return <Canvas draw={draw} />;
+  return (
+    <Canvas draw={draw} width={window.innerWidth} height={window.innerHeight} />
+  );
 }
 
 render(<App />, document.getElementById("root"));
